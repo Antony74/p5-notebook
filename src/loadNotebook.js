84e8c7a769483966e0d30a63fcd397d74183968c
Antony74 <akb@akb.me.uk>
2023-08-18 18:37:59 +0100

    Finish making these notebooks self-contained

--- src/loadNotebook.js
+++ src/loadNotebook.js
@@ -1,4 +1,4 @@
-import { StarboardEmbed } from 'https://cdn.jsdelivr.net/npm/starboard-wrap@0.4.1/dist/index.min.js';
+import { StarboardEmbed } from '../node_modules/starboard-wrap/dist/index.js';
 
 export const loadNotebook = (url, id) => {
     fetch(url, { cache: 'no-cache' })
@@ -6,7 +6,7 @@ export const loadNotebook = (url, id) => {
         .then((notebookContent) => {
             const el = new StarboardEmbed({
                 notebookContent,
-                src: 'https://cdn.starboard.gg/npm/starboard-notebook@0.13.2/dist/index.html',
+                src: '../node_modules/starboard-notebook/dist/index.html',
                 preventNavigationWithUnsavedChanges: true,
                 onUnsavedChangesStatusChange: (t) =>
                     console.log('Unsaved changes status change:', t),

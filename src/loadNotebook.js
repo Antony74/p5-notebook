5de0dce2f9901255ecd769d952a0de22b6f79975
Antony74 <akb@akb.me.uk>
2023-08-22 22:19:01 +0100

    starboard-wrap removal

--- src/loadNotebook.js
+++ src/loadNotebook.js
@@ -1,19 +1,17 @@
-import { StarboardEmbed } from 'https://cdn.jsdelivr.net/npm/starboard-wrap@0.4.1/dist/index.min.js';
-
 export const loadNotebook = (url, id) => {
     fetch(url, { cache: 'no-cache' })
         .then((response) => response.text())
         .then((notebookContent) => {
-            const el = new StarboardEmbed({
-                notebookContent,
-                src: 'https://cdn.starboard.gg/npm/starboard-notebook@0.13.2/dist/index.html',
-                preventNavigationWithUnsavedChanges: true,
-                onUnsavedChangesStatusChange: (t) =>
-                    console.log('Unsaved changes status change:', t),
-                allow: '',
-                sandbox: 'allow-scripts allow-same-origin',
-            });
+            window.initialNotebookContent = notebookContent;
 
-            document.getElementById(id).appendChild(el);
+            const script = document.createElement('script');
+            script.setAttribute('defer', 'defer');
+
+            script.setAttribute(
+                'src',
+                '../lib/starboard-notebook/dist/starboard-notebook.js',
+            );
+
+            document.getElementById(id).appendChild(script);
         });
 };

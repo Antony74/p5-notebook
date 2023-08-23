400096e3530c44a866b43c5663b4389fba88fba9
Antony74 <akb@akb.me.uk>
2023-08-23 20:56:27 +0100

    Contents page

--- build.js
+++ build.js
@@ -9,14 +9,14 @@ copyLibs();
 
 const notebooks = [
     {
-        title: 'Tic-tac-toe',
-        notebookFilename: 'tic-tac-toe.txt',
-        htmlFilename: 'src/tic-tac-toe.html',
-    },
-    {
         title: 'In praise of the map function',
         notebookFilename: 'in-praise-of-the-map-function.txt',
-        htmlFilename: 'src/in-praise-of-the-map-function.html',
+        htmlFilename: 'in-praise-of-the-map-function.html',
+    },
+    {
+        title: 'Tic-tac-toe',
+        notebookFilename: 'tic-tac-toe.txt',
+        htmlFilename: 'tic-tac-toe.html',
     },
 ];
 
@@ -28,10 +28,39 @@ const build = async () => {
 
     notebooks.forEach((notebook) => {
         fsp.writeFile(
-            path.join(__dirname, notebook.htmlFilename),
+            path.join(__dirname, 'src', notebook.htmlFilename),
             mustache.render(template, notebook)
         );
     });
+
+    const header = template.split('</a>')[0].trim();
+
+    const contents = [
+        mustache.render(header, { title: 'Contents' }),
+        '',
+        '</a>',
+        '<starboard-cell class="cell-grid cell-container">',
+        '<div class="cell-gutter cell-gutter-left-top"></div>',
+        '<div></div>',
+        '<div></div>',
+        '<div>',
+        '<br/>',
+        '<h1>p5-notebook</h1>',
+        '<p>Welcome!</p>',
+        '<h2>Contents</h2>',
+        '<ol>',
+        ...notebooks.map(
+            (notebook) =>
+                `<li><a href="${notebook.htmlFilename}">${notebook.title}</a></li>`
+        ),
+        '</ol>',
+        '</div>',
+        '</starboard-cell>',
+        '</body>',
+        '</html>',
+    ].join('\n');
+
+    fsp.writeFile(path.join(__dirname, 'src/index.html'), contents);
 };
 
 build();

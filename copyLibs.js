f1b5aba80bf2991000c7024cc83ef55b0236bfd0
Antony74 <akb@akb.me.uk>
2023-09-17 15:53:46 +0100

    Attempt at build

--- copyLibs.js
+++ copyLibs.js
@@ -40,8 +40,10 @@ const copyDir = async (src, dest) => {
 const copyLibs = async () => {
     const nodeModulesDir = path.join(__dirname, 'node_modules');
     const libDir = path.join(__dirname, 'lib');
+    const distDir = path.join(__dirname, 'dist');
 
     await rimraf(libDir);
+    await rimraf(distDir);
     await fsp.mkdir(libDir);
     await fsp.mkdir(path.join(libDir, 'github-fork-ribbon-css'));
     await fsp.mkdir(path.join(libDir, 'coi-serviceworker'));
